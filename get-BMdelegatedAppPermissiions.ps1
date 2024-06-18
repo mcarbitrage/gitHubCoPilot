68f86fe47b20546c1ab4bc7ddb368a70bf9e5de4
mcarbitrage <mcarbitrage@users.noreply.github.com>
2024-06-18 16:35:41 +0100

    tidyup01

--- get-BMdelegatedAppPermissiions.ps1
+++ get-BMdelegatedAppPermissiions.ps1
@@ -1,7 +1,7 @@
 
 $tenantid = "6db24f64-e885-4320-9179-7789185df423"
 Connect-MgGraph -$TenantId 
-write-host "Objet ID of Graph Explorer"
+write-host "Object ID of Graph Explorer"
 # $objectId = Read-host "Enter the <Object ID of the Enterprise Application>"
 $objectId = "a155ae2d-d1ee-4098-800c-940567cb18f6"
 Get-MgServicePrincipalOauth2PermissionGrant -Filter "ResourceId eq '$objectId'" | Format-Table -Property Id,ClientId,ConsentType,PrincipalId,ResourceId,Scope
